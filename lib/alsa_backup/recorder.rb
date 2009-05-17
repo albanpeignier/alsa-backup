4cc349382a9750331dace8691d5fd35495340a6c
Alban Peignier <alban.peignier@free.fr>
2009-05-17 13:08:01 +0200

    Use read frame count instead of Time.now to limit record

--- lib/alsa_backup/recorder.rb
+++ lib/alsa_backup/recorder.rb
@@ -11,10 +11,10 @@ module AlsaBackup
         ALSA::PCM::Capture.new.open("hw:0") do |capture|
           capture.hardware_parameters = format.merge(:sample_format => :s16_le)
 
-          start = Time.now
+          frames_to_read = format[:sample_rate] * 2
           capture.read do |buffer, frame_count|
             file.write buffer, frame_count
-            (Time.now - start) < 2
+            (frames_to_read -= frame_count) > 0
           end
         end
       end
